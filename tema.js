51bc3b4f6b4df00297e667a7edc96ff450051e62
WeslleyNsou <120567284+WeslleyNsou@users.noreply.github.com>
2025-04-01 12:39:10 -0300

    mudança da cor das letras por meio do javascript

--- tema.js
+++ tema.js
@@ -1,5 +1,13 @@
 var tema = document.getElementById('button-tema')
 
 tema.addEventListener('click', function(){
-    var 
+// querySelectorAll seleciona todos os elementos 
+// e armazena na variável textEscuro
+    var textEscuro = document.querySelectorAll('a, h1, h2, p, li')
+//toggle a classe dark em todos os elementos selecionados
+    //para que o texto fique claro quando o tema escuro for ativado
+    textEscuro.forEach(function(textEscuro){
+        textEscuro.classList.toggle('dark')
+    })
+
 })
